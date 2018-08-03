129dbed39e0eb2d5328588d2a07458aea63c3923
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-08-03 06:41:53 -0300

    initial commit

---
+++ src/app/components/template-driven/template-driven.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven',
+  templateUrl: './template-driven.component.html',
+  styleUrls: ['./template-driven.component.css']
+})
+export class TemplateDrivenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
